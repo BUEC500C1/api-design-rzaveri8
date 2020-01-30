9baa1611593bfd3405cfad3bba51a40c01b30dc6
Ruby <rzaveri@bu.edu>
2020-01-30 12:59:15 -0500

    return-location

--- weather-api.py
+++ weather-api.py
@@ -1,14 +1,28 @@
-import tweepy
+#!/usr/bin/python
+#TO DO
+#INPUT CHECKS
+#CHECK IF VALID ISO CODE
+#CHECK IF STRING
 
-# Let's say this is a web app, so we need to re-build the auth handler
-# first...
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-token = session.get('request_token')
-session.delete('request_token')
-auth.request_token = { 'oauth_token' : token,
-                         'oauth_token_secret' : verifier }
+import csv
+import sys
+"""
+#input number you want to search
+code = raw_input('Enter number to find\n')
 
-try:
-    auth.get_access_token(verifier)
-except tweepy.TweepError:
-    print('Error! Failed to get access token.')
+#read csv, and split on "," the line
+csv_file = csv.reader(open('airport-codes.csv', "rb"), delimiter=",")
+
+
+#loop through csv list
+for row in csv_file:
+    #if current rows 2nd value is equal to input, print that row
+    if code == row[1][1]:
+         print row
+"""
+
+#row_number, column_number = [int(arg, 10)-1 for arg in sys.argv[2:])]
+
+with open('airport-codes.csv', 'rb') as f:
+     rows = list(csv.reader(f))
+     print rows[1][0]
