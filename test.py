bd8af60c3c406dda9787446f38ac6406e78a57fe
Ruby <rzaveri@bu.edu>
2020-02-07 12:43:54 -0500

    update-tests

--- test.py
+++ test.py
@@ -1,30 +1,29 @@
 import weather_api
 import pytest
 import sample_config
+from helper_funcs import getAirportLoc
+from helper_funcs import getCity
 
 
 
 # content of test_class.py
-#tests. 
+#tests.
 class TestClass:
     # tests for airport code input
     def test_airport_exist(self):
-        result = hasattr(weather_api, 'getAirportLoc')
+        result = hasattr(helper_funcs, 'getAirportLoc')
         assert result, "Function does not exist!"
     def test_airport_correct(self):
-        result = weather_api.getAirportLoc('AES')
+        result = getAirportLoc('AES')
         assert result =='Aalesund, Norway '
     def test_airport_false(self):
-        result = weather_api.getAirportLoc(7)
+        result = getAirportLoc(7)
         assert result == "ERROR: Not a String"
 
     #tests for weather api output
-    def test_weather_exist(self):
-        result = hasattr(weather_api, 'getWeatherSummary')
+    def test_city_exist(self):
+        result = hasattr(helper_funcs, 'getCity')
         assert result, "Function does not exist!"
-    def test_weather_correct(self):
-        result = weather_api.getWeatherSummary('adsfsdfdsfs')
-        assert result =="ERROR: City not found, please try again"
-    def test_weather_false(self):
-        result = weather_api.getWeatherSummary(7)
-        assert result == "ERROR: Not a string"
+    def test_city_correct(self):
+        result = getCity(42)
+        assert result =="ERROR: Not a String"
--- weather_api.py
+++ weather_api.py
@@ -13,37 +13,9 @@ import flask
 import json
 from flask import jsonify
 import sample_config
+from helper_funcs import getAirportLoc
+from helper_funcs import getCity
 
-
-def getAirportLoc(code):
-    check = isinstance(code,str)
-    while check == False:
-        return "ERROR: Not a String"
-        break
-    mycode = code
-    with open('airport-codes.csv', 'rb') as f:
-        rows = list(csv.reader(f))
-        csv_file_reader = csv.DictReader(f)
-        for i in range(1,3592): #this goes through all of the rows
-            for j in range(0,2): #checks each colomn
-                if mycode == rows[i][j]:
-                    loc = str(rows[i][j-1])
-                    #print "thisi is LOc" , loc
-                    return loc
-            else:
-                return "Airport Code not found, please try again"
-
-#parsing out just the city
-def getCity(loc):
-    check = isinstance(loc,str)
-    while check == False:
-        return "ERROR: Not a String"
-        break
-    my_string = loc
-    result = [x.strip() for x in my_string.split(',')]
-    city = result[0]
-    #print city
-    return city
 
 def getWeatherSummary(city):
     key = sample_config.api_key()
